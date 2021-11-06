2243e63031fd12e9ec35ec79543605c7970ce051
Anh Khoa <anhkhoahuynh90@gmail.com>
2021-11-06 09:15:56 +0700

    change package for comparator

--- src/java/app/feedback/Comparator.java
+++ src/java/app/feedback/Comparator.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package app.users;
+package app.feedback;
+
+import app.users.UserHistoryDTO;
 
 /**
  *
